5a2a3c5f5283d9dbe9da5854edc15c616b967e2b
Karol Wrótniak <koral--@users.noreply.github.com>
2015-07-31 02:56:07 +0200

    unit test added

--- buildSrc/src/test/groovy/pl/droidsonroids/domainnameutils/TldListGeneratorTest.groovy
+++ buildSrc/src/test/groovy/pl/droidsonroids/domainnameutils/TldListGeneratorTest.groovy
@@ -1,15 +1,27 @@
 package pl.droidsonroids.domainnameutils
 
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue
 
 public class TldListGeneratorTest {
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder()
 
     @Test
-    public void test() {
+    public void testSavedSource() {
         def savedSourceTxt = TldListGenerator.class.getResource('tlds-alpha-by-domain.txt').text
         assertTrue(savedSourceTxt.startsWith('#'))
         assertTrue(savedSourceTxt.contains('COM'))
+    }
+
+    @Test
+    public void testGenerator() {
+        def outputDir = tempFolder.newFolder()
+        TldListGenerator.generateTldListClass(outputDir.getPath(), true)
+        def generatedFile = new File(outputDir, 'pl/droidsonroids/domainnameutils/TldList.java')
+        assertTrue(generatedFile.isFile())
     }
 }
